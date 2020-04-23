2d2a5d06154b3c595307e20c34c8e78b9aad57b2
xcoldbloom <61301318+xcoldbloom@users.noreply.github.com>
2020-04-23 18:14:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,104 +1,20 @@
-const projectName = "random-quote-machine";
-localStorage.setItem('example_project', 'Randowm Quote Machine');
-let quotesData;
-
-/*
-  Code by Gabriel Nunes
-  Modified by Todd Chaffee to use Camper gist for JSON Quote data.
-*/
-
-function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
-
-var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-var currentQuote = '', currentAuthor = '';
-function openURL(url){
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
-}
-
-function getQuotes() {
-  return $.ajax({
-    headers: {
-      Accept: "application/json"
-    },
-    url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
-    success: function(jsonQuotes) {
-      if (typeof jsonQuotes === 'string') {
-        quotesData = JSON.parse(jsonQuotes);
-        console.log('quotesData');
-        console.log(quotesData);
-      }
-    }
-  });
-}
-
-function getRandomQuote() {
-  return quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
-}
-
-function getQuote() {
-
-  let randomQuote = getRandomQuote();
+window.onload = function(){
+  //array with the words
+  var first = 'A ';
+  var adj = ['retarded ', 'shit-for-brain ', 'tyranical ', 'miserable ','crazed ','hideious  ','smelly ','fat ','senile ' ];
+  var noun = ['cunt ','retard ','rapist ', 'cocksucker ', 'maniac ', 'motherfucker ', 'loser ', 'sewer orphan ','ginger ', 'banshee ', 'fuck-head ', 'fuck-ass '];
+  var action = ['blew up ', 'killed my ', 'raped my ' , 'kicked my ', 'stole my ', 'burned my ', 'bit my ', 'desolated my ', 'sucked my ', 'banged my '];
+  var possetion = ['biscuit ', 'bike ', 'kid ', 'dog ', 'youth ', 'life ', 'children ', 'cunt ', 'girl ', 'monkey ', 'ass ', 'cat '];
+  var where = ['at the beach','on some gang shii','at the park', 'at walmart', 'in the sewers', 'outside', 'in the fourth dimension', 'fr', 'on some fuck shii'];
   
-  currentQuote = randomQuote.quote;
-  currentAuthor = randomQuote.author;
-
-  if(inIframe())
-  {
-    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-
-    $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
-  }
+  // declaring random variables
+   var rdm1 = Math.floor((Math.random() * adj.length) );
+   var rdm2 = Math.floor((Math.random() * noun.length) );
+   var rdm3 = Math.floor((Math.random() * action.length) );
+   var rdm4 = Math.floor((Math.random() * possetion.length) );
+   var rdm5 = Math.floor((Math.random() * where.length) );
   
-  $(".quote-text").animate(
-    { opacity: 0 },
-    500,
-    function() {
-      $(this).animate({ opacity: 1}, 500);
-      $('#text').text(randomQuote.quote);
-    }
-  );
-
-  $(".quote-author").animate(
-    { opacity: 0 },
-    500,
-    function() {
-      $(this).animate({ opacity: 1}, 500);
-      $('#author').html(randomQuote.author);
-    }
-  );
-
-  var color = Math.floor(Math.random() * colors.length);
-  $("html body").animate(
-    {
-      backgroundColor: colors[color],
-      color: colors[color]
-    },
-    1000
-  );
-  $(".button").animate(
-    {
-      backgroundColor: colors[color]
-    },
-    1000
-  );
+    //creating a sentence (the excuse)
+    document.querySelector('#theexcuse').innerHTML = first + adj[rdm1] + noun[rdm2] + action[rdm3] + possetion[rdm4] + where[rdm5]; 
+  
 }
-
-$(document).ready(function() {
-  getQuotes().then(() => {
-    getQuote();
-  });
-
-  $('#new-quote').on('click', getQuote);
-
-  $('#tweet-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-    }
-  });
-
-  $('#tumblr-quote').on('click', function() {
-    if(!inIframe()) {
-      openURL('https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
-    }
-  });
-});
