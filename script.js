2a93f4929e5aad9def48f8bde3bcaf4939050b38
xcoldbloom <61301318+xcoldbloom@users.noreply.github.com>
2020-04-23 18:38:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function(){
   //array with the words
   var first = 'A ';
-  var adj = ['retarded ', 'shit-for-brain ', 'tyranical ', 'miserable ','crazed ','hideious  ','smelly ','fat ','senile ' ];
-  var noun = ['cunt ','retard ','rapist ', 'cocksucker ', 'maniac ', 'motherfucker ', 'loser ', 'sewer orphan ','ginger ', 'banshee ', 'fuck-head ', 'fuck-ass '];
+  var adj = ['retarded ', 'shit-for-brain ', 'tyranical ', 'miserable ','crazed ','hideious  ','smelly ','fat ','senile ', 'good-for-nothing ', 'dispicable ' ];
+  var noun = ['cunt ','retard ','rapist ', 'whore ', 'cocksucker ', 'maniac ', 'motherfucker ', 'loser ', 'sewer orphan ','ginger ', 'banshee ', 'fuck-head ', 'fuck-ass '];
   var action = ['blew up ', 'killed my ', 'raped my ' , 'kicked my ', 'stole my ', 'shot my ', 'ate my ', 'desolated my ', 'sucked my ', 'banged my ', 'did it for tha '];
   var possetion = ['biscuit ', 'bike ', 'kid ', 'dog ', 'youth ', 'life ', 'children ', 'cunt ', 'girl ', 'monkey ', 'ass ', 'cat '];
   var where = ['at the beach','on some gang shii','at the park', 'at walmart', 'in the sewers', 'outside', 'in the fourth dimension', 'on god', 'fr', 'on some fuck shii', '', '', '', '', '', '', '', '', '', '', '', ''];
