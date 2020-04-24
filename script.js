7502936026957a7b1b6c22fc27fcc42179bc3f8d
xcoldbloom <61301318+xcoldbloom@users.noreply.github.com>
2020-04-24 02:47:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function(){
   var adj = ['retarded ', 'dirty little ', 'kold-blooded ', 'shit-for-brain ', 'tyranical ', 'bitter ', 'miserable ', 'leftist ', 'misanthropic ', 'cis-gendered ', 'crazed ','hideious  ','smelly ','fat ','senile ', 'good-for-nothing ', 'dispicable '];
   var noun = ['cunt ','retard ','rapist ', 'psycho ', 'democrat ', 'jew ', 'antifa ', 'whore ', 'feminist ', "women's rights activist ", 'SJW ', 'cocksucker ', 'maniac ', 'motherfucker ', 'loser ', 'sewer orphan ','ginger ', 'banshee ', 'fuck-ass '];
   var action = ['blew up my ', 'killed my ', 'raped my ', 'broke my ', 'kicked my ', 'twerked on my ', 'listened to my ', 'whispered to my ', 'stomped my ', 'stole my ', 'shot my ', 'rode my ', 'ate my ', 'shidded on my ', 'desolated my ', 'sucked my ', 'banged my ', 'did it for tha '];
-  var possetion = ['biscuits ', 'bike ', 'kid ', 'dog ', 'youth ', 'mind ', 'hopes and dreams ', 'sanity ', 'life ', 'children ', 'cunt ', 'girl ', 'ass ', 'cat '];
+  var possetion = ['biscuits ', 'bike ', 'kids ', 'dog ', 'youth ', 'mind ', 'hopes and dreams ', 'sanity ', 'life ', 'children ', 'cunt ', 'girl ', 'ass ', 'cat '];
   var where = ['at the beach','at the park', 'on the curb ', 'outside', '-- now THATs gansta!', 'in the fourth dimension', 'on god', 'fr', '', ' WTF', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
   
   // declaring random variables
