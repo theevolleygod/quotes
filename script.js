ed8e3bbbbbf119324624da700413e5a2552d3b10
xcoldbloom <61301318+xcoldbloom@users.noreply.github.com>
2020-04-23 22:25:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function(){
   //array with the words
   var first = 'A ';
   var adj = ['retarded ', 'dirty little ', 'kold-blooded ', 'shit-for-brain ', 'tyranical ', 'bitter ', 'miserable ', 'leftist ', 'misanthropic ', 'cis-gendered ', 'crazed ','hideious  ','smelly ','fat ','senile ', 'good-for-nothing ', 'dispicable '];
-  var noun = ['cunt ','retard ','rapist ', 'psycho ', 'democrat ', 'jew ', 'antifa ', 'whore ', 'feminist ', "women's rights activist ", 'SJW ', 'cocksucker ', 'maniac ', 'motherfucker ', 'loser ', 'sewer orphan ','ginger ', 'banshee ', 'fuck-head ', 'fuck-ass '];
+  var noun = ['cunt ','retard ','rapist ', 'psycho ', 'democrat ', 'jew ', 'antifa ', 'whore ', 'feminist ', "women's rights activist ", 'SJW ', 'cocksucker ', 'maniac ', 'motherfucker ', 'loser ', 'sewer orphan ','ginger ', 'banshee ', 'fuck-ass '];
   var action = ['blew up my ', 'killed my ', 'raped my ', 'broke my ', 'kicked my ', 'twerked on my ', 'listened to my ', 'whispered to my ', 'stomped my ', 'stole my ', 'shot my ', 'rode my ', 'ate my ', 'shidded on my ', 'desolated my ', 'sucked my ', 'banged my ', 'did it for tha '];
   var possetion = ['biscuits ', 'bike ', 'kid ', 'dog ', 'youth ', 'mind ', 'hopes and dreams ', 'sanity ', 'life ', 'children ', 'cunt ', 'girl ', 'ass ', 'cat '];
   var where = ['at the beach','at the park', 'on the curb ', 'outside', '-- now THATs gansta!', 'in the fourth dimension', 'on god', 'fr', '', ' WTF', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
