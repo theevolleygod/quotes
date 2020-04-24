fe7497f8a8e9760ef33fe72cb34eed7fa39b6721
xcoldbloom <61301318+xcoldbloom@users.noreply.github.com>
2020-04-23 21:41:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.onload = function(){
   var noun = ['cunt ','retard ','rapist ', 'psycho ', 'democrat ', 'jew ', 'antifa ', 'whore ', 'feminist ', "women's rights activist ", 'SJW ', 'cocksucker ', 'maniac ', 'motherfucker ', 'loser ', 'sewer orphan ','ginger ', 'banshee ', 'fuck-head ', 'fuck-ass '];
   var action = ['blew up my ', 'killed my ', 'raped my ', 'broke ', 'kicked my ', 'twerked on my ', 'listened to my ', 'whispered to my ', 'stomped my ', 'stole my ', 'shot my ', 'rode my ', 'ate my ', 'shidded on my ', 'desolated my ', 'sucked my ', 'banged my ', 'did it for tha '];
   var possetion = ['biscuits ', 'bike ', 'kid ', 'dog ', 'youth ', 'mind ', 'hopes and dreams ', 'sanity ', 'life ', 'children ', 'cunt ', 'girl ', 'ass ', 'cat '];
-  var where = ['at the beach',', on some gang shii','at the park', 'on the curb ', 'in the sewers', 'outside', '-- now THATs gansta!', 'in the fourth dimension', 'on god', 'fr', '', ' WTF', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
+  var where = ['at the beach','at the park', 'on the curb ', 'outside', '-- now THATs gansta!', 'in the fourth dimension', 'on god', 'fr', '', ' WTF', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
   
   // declaring random variables
    var rdm1 = Math.floor((Math.random() * adj.length) );
