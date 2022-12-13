9ec1032f6c65b687eaca9089dd23f8b6d65c366f
bobby <61301318+theevolleygod@users.noreply.github.com>
2022-12-12 21:54:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function(){
   //array with the words
   var first = 'A ';
   var adj = ['retarded ', 'dirty little ', 'kold-blooded ', 'shit-for-brain ', 'tyranical ', 'bitter ', 'miserable ', 'leftist ', 'bigotted ', 'cis-gendered ', 'crazed ','hideious ','smelly ','fat ','senile ', 'good-for-nothing ', 'dispicable '];
-  var noun = ['cunt ','retard ','rapist ', 'psycho ', 'democrat ', 'jew ', 'antifa ', 'whore ', 'feminist ', "women's rights activist ", 'SJW ', 'cocksucker ', 'maniac ', 'mofugga ', 'loser ', 'mudshark', 'sewer orphan ','hebephile ', 'bitch nigga ', 'real nigga', 'fuck-ass '];
+  var noun = ['cunt ','retard ','rapist ', 'psycho ', 'democrat ', 'jew ', 'antifa ', 'whore ', 'feminist ', "women's rights activist ", 'SJW ', 'cocksucker ', 'maniac ', 'mofugga ', 'loser ', 'mudshark', 'sewer orphan ','hebephile ', 'bitch nigga ', 'real nigga ', 'fuck-ass '];
   var action = ['blew up my ', 'killed my ', 'raped my ', 'broke my ', 'kicked my ', 'twerked on my ', 'listened to my ', 'whispered to my ', 'stomped my ', 'stole my ', 'shot my ', 'rode my ', 'ate my ', 'shitted on my ', 'desolated my ', 'sucked my ', 'banged my ', 'did it for tha '];
   var possetion = ['biscuits ', 'bike ', 'kids ', 'dog ', 'youth ', 'mind ', 'hopes and dreams ', 'sanity ', 'life ', 'children ', 'cunt ', 'girl ', 'ass ', 'cat ', 'gang shit'];
   var where = ['at the beach','at the park', 'on the curb ', 'outside', '-- now THATs gansta!', 'in the fourth dimension', 'on god', 'fr', '', ' WTF', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''];
